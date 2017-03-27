10472be07264673765618161195fecd44e433be1
“chinx <c54948054@126.com>
2017-03-27 19:53:08 +0800

    Benchmark test

--- web/trie_test.go
+++ web/trie_test.go
@@ -6,146 +6,111 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/chinx/mohist/web/nrouter"
+	"github.com/bmizerany/pat"
+	"github.com/gin-gonic/gin"
+	"github.com/go-baa/baa"
 	"github.com/go-macaron/macaron"
+	"github.com/julienschmidt/httprouter"
+	"github.com/labstack/echo"
 )
 
-func BenchmarkBytes(b *testing.B) {
-	//f, err := os.OpenFile("./new/bytes", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	//if err != nil {
-	//	b.Fatal(err)
-	//}
-	//log.SetOutput(f)
-	//n := nrouter.NewNode()
-	//paths, logs := addUrl()
-	//for i := 0; i < len(paths); i++ {
-	//	addBytes(n, []byte(paths[i]), logs[i])
-	//}
-
-	//for i := 0; i < b.N; i++ {
-	//	matches := matchUrl()
-	//	for i := 0; i < len(matches); i++ {
-	//		matchBytes(n, []byte(paths[i]), &url.Values{})
-	//	}
-	//}
-	//f.Close()
-
-	a := []byte("abc")
-	log.Println(string(a[:1]))
-	log.Println(string(a))
+type testPat struct {
 }
 
-func BenchmarkMatch(b *testing.B) {
-	//f, err := os.OpenFile("./new/self", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	//if err != nil {
-	//	b.Fatal(err)
-	//}
-	//log.SetOutput(f)
-	n := newNode()
-	paths, logs := addUrl()
-	for i := 0; i < len(paths); i++ {
-		addSelf(n, paths[i], logs[i])
-	}
+func (p testPat) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
-	matches := matchUrl()
-	for i := 0; i < b.N; i++ {
-		for i := 0; i < len(matches); i++ {
-			matchSelf(n, matches[i], &url.Values{})
-		}
-	}
-	//f.Close()
+}
+
+func BenchmarkMohist(b *testing.B) {
+	n := NewRouter()
+	execute(b, n, func(path string) {
+		n.Get(path, func(w http.ResponseWriter, req *http.Request, params *url.Values) {
+		})
+	})
+}
+
+func BenchmarkEach(b *testing.B) {
+	n := echo.New()
+	execute(b, n, func(path string) {
+		n.GET(path, func(content *echo.Context) {
+		})
+	})
+}
+
+func BenchmarkGin(b *testing.B) {
+	n := gin.New()
+	execute(b, n, func(path string) {
+		n.GET(path, func(content *gin.Context) {
+		})
+	})
+}
+
+func BenchmarkBaa(b *testing.B) {
+	n := baa.New()
+	execute(b, n, func(path string) {
+		n.Get(path, func(content *baa.Context) {
+		})
+	})
+}
+
+func BenchmarkPat(b *testing.B) {
+	n := pat.New()
+	execute(b, n, func(path string) {
+		n.Get(path, &testPat{})
+	})
+}
+
+func BenchmarkHttpRouter(b *testing.B) {
+	n := &httprouter.Router{}
+	execute(b, n, func(path string) {
+		n.GET(path, func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {})
+	})
 }
 
 func BenchmarkMacaron(b *testing.B) {
-	//f, err := os.OpenFile("./new/macaron", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	//if err != nil {
-	//	b.Fatal(err)
-	//}
-	//log.SetOutput(f)
-	n := macaron.NewTree()
+	n := macaron.New()
+	execute(b, n, func(path string) {
+		n.Get(path, func(w http.ResponseWriter, req *http.Request) {})
+	})
+}
 
-	addUrls, logs := addUrl()
+func execute(b *testing.B, n http.Handler, handler func(string)) {
+	addUrls := addUrl()
 	for i := 0; i < len(addUrls); i++ {
-		addMacaron(n, addUrls[i], logs[i])
+		handler(addUrls[i])
 	}
-
 	matches := matchUrl()
 	for i := 0; i < b.N; i++ {
 		for i := 0; i < len(matches); i++ {
-			matchMacaron(n, matches[i])
+			request(n, matches[i])
 		}
 	}
-	//f.Close()
 }
 
-func addBytes(n *nrouter.Node, path []byte, logs string) {
-	n.Add(path, func(w http.ResponseWriter, req *http.Request, params *url.Values) {
-		log.Println(logs)
-	})
-}
-
-func matchBytes(n *nrouter.Node, path []byte, params *url.Values) {
-	if handler := n.Match(path, params); handler != nil {
-		handler(nil, nil, nil)
-		log.Println(params)
+func request(n http.Handler, path string) {
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		log.Println(err)
 	}
+	n.ServeHTTP(nil, req)
 }
 
-func addSelf(n *node, path, logs string) {
-	n.add(path, func(w http.ResponseWriter, req *http.Request, params *url.Values) {
-		//log.Println(logs)
-	})
-}
-
-func matchSelf(n *node, path string, params *url.Values) {
-	if handler := n.match(path, params); handler != nil {
-		handler(nil, nil, nil)
-		//log.Println(params)
-	}
-}
-
-func addMacaron(n *macaron.Tree, path, logs string) {
-	n.Add(path, func(w http.ResponseWriter, req *http.Request, params macaron.Params) {
-		//log.Println(logs)
-	})
-}
-
-func matchMacaron(n *macaron.Tree, path string) {
-	if handler, params, ok := n.Match(path); ok {
-		handler(nil, nil, params)
-		//log.Println(params)
-	}
-
-}
-
-func addUrl() ([]string, []string) {
+func addUrl() []string {
 	return []string{
-			"/accounts/",
-			"/accounts/:account",
-			"/accounts/:account/projects",
-			"/accounts/:account/projects/:project",
-			"/accounts/:account/projects/abcde",
-			"/accounts/:account/projects/:project/files/?:file",
-		}, []string{
-			"one",
-			"two",
-			"three",
-			"four",
-			"five",
-			"six",
-		}
+		"/accounts/",
+		"/accounts/:account",
+		"/accounts/:account/projects",
+		"/accounts/:account/projects/:project",
+		"/accounts/:account/projects/:project/files/:file",
+	}
 }
 
 func matchUrl() []string {
 	return []string{
 		"/accounts/",
 		"/accounts/account",
-		"/accounts/abc/projects",
+		"/accounts/account/projects",
 		"/accounts/account/projects/project",
-		"/accounts/account/projects/abcde",
-		"/acc/account/projects/abcde",
-		"/acc/account/projects/project/files",
-		"/acc/account/projects/project/files/file",
-		"/acc/account/projects/project/files/file/1",
+		"/accounts/account/projects/project/files/file",
 	}
 }
